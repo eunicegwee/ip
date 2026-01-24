d7516adcd89be0bf32693df896465ee045d116e0
Eunice Gwee <eunicegweee@gmail.com>
2026-01-24 19:23:26 +0800

    Added mark/unmark as done feature

--- src/main/java/Kira.java
+++ src/main/java/Kira.java
@@ -31,14 +31,46 @@ public class Kira {
 
             if (command.equals("bye")) {
                 break;
-            } else if (command.equals("list")) {
+            }
+
+            // COMMAND: list
+            else if (command.equals("list")) {
                 System.out.println(line);
+                System.out.println("Here are all your TASKS!");
                 for (int i = 0; i < taskCount; i++) {
-                    // Uses the accessor methods from Task.java
-                    System.out.println(" " + (i + 1) + ". " + tasks[i].getDescription());
+                    System.out.println(" " + (i + 1) + "." + tasks[i].toString());
                 }
                 System.out.println(line);
-            } else {
+            }
+
+            // COMMAND: mark
+            else if (command.startsWith("mark")) {
+                String[] parts = command.split(" ");
+                int index = Integer.parseInt(parts[1]) - 1;
+
+                tasks[index].markAsDone();
+
+                System.out.println(line);
+                System.out.println(" Yay! Task marked as done:");
+                System.out.println("   " + tasks[index].toString());
+                System.out.println(line);
+            }
+
+            // COMMAND: unmark
+
+            else if (command.startsWith("unmark")) {
+                String[] parts = command.split(" ");
+                int index = Integer.parseInt(parts[1]) - 1;
+
+                tasks[index].markAsUndone();
+
+                System.out.println(line);
+                System.out.println(" Okay... got it, not done yet:");
+                System.out.println("   " + tasks[index].toString());
+                System.out.println(line);
+            }
+
+            else {
                 tasks[taskCount] = new Task(command);
                 taskCount++;
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -7,7 +7,20 @@ public class Task {
         this.isDone = false;
     }
 
-    public String getDescription() {
-        return description;
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsUndone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
     }
 }
