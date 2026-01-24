5ed9750a79f219423ff6d452004dd91d6fb9715b
Eunice Gwee <eunicegweee@gmail.com>
2026-01-24 18:47:44 +0800

    Level-1

--- src/main/java/Kira.java
+++ src/main/java/Kira.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class Kira {
     public static void main(String[] args) {
-        String horizontalLine = "____________________________________________________________";
+        String line = "____________________________________________________________";
         String logo = """
                     __ __  _           \s
                    / //_/ (_)_________ \s
@@ -12,13 +14,31 @@ public class Kira {
         System.out.println("Hello from\n" + logo);
 
         // Greeting
-        System.out.println(horizontalLine);
+        System.out.println(line);
         System.out.println(" Hello! I'm Kira!");
         System.out.println(" How can I help you today?");
-        System.out.println(horizontalLine);
+        System.out.println(line);
+
+        // Initialize scanner
+        Scanner in = new Scanner(System.in);
+        String command;
+
+        while (true) {
+            command = in.nextLine();
+
+            if (command.equals("bye")) {
+                break;
+            }
+
+            // Echo the command
+            System.out.println(line);
+            System.out.println(" " + command);
+            System.out.println(line);
+        }
 
         // Exit
+        System.out.println(line);
         System.out.println(" Adios. See you later!");
-        System.out.println(horizontalLine);
+        System.out.println(line);
     }
 }
