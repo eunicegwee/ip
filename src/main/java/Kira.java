e5d960759df58671ec8945e96584ec1ad91399e4
Eunice Gwee <eunicegweee@gmail.com>
2026-01-24 21:15:35 +0800

    Switched to using ArrayList to store tasks

--- src/main/java/Kira.java
+++ src/main/java/Kira.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Kira {
     public static void main(String[] args) {
@@ -23,8 +24,7 @@ public class Kira {
         Scanner in = new Scanner(System.in);
         String command;
 
-        Task[] tasks = new Task[100];
-        int taskCount = 0;
+        ArrayList<Task> tasks = new ArrayList<>();
 
         while (true) {
             try {
@@ -39,8 +39,8 @@ public class Kira {
                 else if (command.equals("list")) {
                     System.out.println(line);
                     System.out.println("Here are all your TASKS!");
-                    for (int i = 0; i < taskCount; i++) {
-                        System.out.println(" " + (i + 1) + "." + tasks[i].toString());
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println(" " + (i + 1) + "." + tasks.get(i).toString());
                     }
                 }
 
@@ -51,13 +51,13 @@ public class Kira {
                         throw new KiraException("OOPS! Please specify the task number.");
                     }
                     int index = Integer.parseInt(parts[1]) - 1;
-                    if (index < 0 || index >= taskCount) {
+                    if (index < 0 || index >= tasks.size()) {
                         throw new KiraException("OOPS! There doesn't seem to be a task " + parts[1] + "!");
                     }
-                    tasks[index].markAsDone();
+                    tasks.get(index).markAsDone();
                     System.out.println(line);
                     System.out.println(" Yay! Task marked as done:");
-                    System.out.println("   " + tasks[index].toString());
+                    System.out.println("   " + tasks.get(index).toString());
                 }
 
                 // COMMAND: unmark
@@ -67,28 +67,24 @@ public class Kira {
                         throw new KiraException("OOPS! Please specify the task number.");
                     }
                     int index = Integer.parseInt(parts[1]) - 1;
-                    if (index < 0 || index >= taskCount) {
+                    if (index < 0 || index >= tasks.size()) {
                         throw new KiraException("OOPS! There doesn't seem to be a task " + parts[1] + "!");
                     }
-                    tasks[index].markAsUndone();
+                    tasks.get(index).markAsUndone();
                     System.out.println(line);
                     System.out.println(" Okay... got it, not done yet:");
-                    System.out.println("   " + tasks[index].toString());
+                    System.out.println("   " + tasks.get(index).toString());
                 }
 
                 // COMMAND: add task (todo/deadline/event)
                 else {
                     Task newTask = getTask(command);
+                    tasks.add(newTask);
 
-                    if (newTask != null) {
-                        tasks[taskCount] = newTask;
-                        taskCount++;
-
-                        System.out.println(line);
-                        System.out.println(" YAY! Task added:");
-                        System.out.println("   " + newTask);
-                        System.out.println(" Now you have " + taskCount + " TASKS in the list!");
-                    }
+                    System.out.println(line);
+                    System.out.println(" YAY! Task added:");
+                    System.out.println("   " + newTask);
+                    System.out.println(" Now you have " + tasks.size() + " TASKS in the list!");
                 }
             } catch (KiraException e) {
                 System.out.println(" " + e.getMessage());
@@ -106,14 +102,12 @@ public class Kira {
     }
 
     private static Task getTask(String command) throws KiraException {
-        Task newTask = null;
-
         if (command.startsWith("todo")) {
             if (command.length() <= 5) {
                 throw new KiraException("OOPS! Please provide a todo description!");
             }
             String description = command.substring(5);
-            newTask = new ToDo(description);
+            return new ToDo(description);
         } else if (command.startsWith("deadline")) {
             if (command.length() <= 9) {
                 throw new KiraException("OOPS! Please provide a deadline description!");
@@ -122,7 +116,7 @@ public class Kira {
             if (parts.length < 2) {
                 throw new KiraException("OOPS! Please add a deadline time (use /by).");
             }
-            newTask = new Deadline(parts[0], parts[1]);
+            return new Deadline(parts[0], parts[1]);
         } else if (command.startsWith("event")) {
             if (command.length() <= 6) {
                 throw new KiraException("OOPS! Please provide a deadline description!");
@@ -136,11 +130,9 @@ public class Kira {
             if (timeParts.length < 2) {
                 throw new KiraException("OOPS! Please add an event end time (use /to).");
             }
-            newTask = new Event(description, timeParts[0], timeParts[1]);
+            return new Event(description, timeParts[0], timeParts[1]);
         } else {
             throw new KiraException("Errr I'm afraid that's not a valid command...");
         }
-
-        return newTask;
     }
 }
